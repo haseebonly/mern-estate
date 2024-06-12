c82dda87afe0672638b921fd7918c1390760e0d1
haseebonly <107769598+haseebonly@users.noreply.github.com>
2024-06-12 07:19:00 +0500

    install react js and tailwind css and create first template

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+    <h1 className="flex justify-center text-3xl font-bold underline bg-slate-400 text-red-500 p-4">
+    Hello world!
+  </h1>
+ </div>
+  )
+}
+
+export default App
