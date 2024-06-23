3ee5d01fc15302324d3139a91884972684cffc71
haseebonly <107769598+haseebonly@users.noreply.github.com>
2024-06-23 23:39:23 +0500

    complete sign in page functionality

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -7,13 +7,13 @@ import jwt from 'jsonwebtoken';
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
+  const hashedPassword = bcryptjs.hashSync(password, 10);
+  const newUser = new User({ username, email, password: hashedPassword });
   try {
-    console.log("Signup request received with:", { username, email, password });
-    const newUser = new User({ username, email, password });
     await newUser.save();
-    res.status(201).json("User created successfully! Nice to Coder");
+    res.status(201).json('User created successfully!');
   } catch (error) {
-    next(errorHandler(550, "error from the function"));
+    next(error);
   }
 };
 
