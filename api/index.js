f56fb88d28e8088da22df4ec331a1db752240961
haseebonly <107769598+haseebonly@users.noreply.github.com>
2024-07-01 00:22:55 +0500

    create user update API

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import { configDotenv } from "dotenv";
 import userRoutes from './routes/user.route.js'; 
 import authRoutes from './routes/auth.route.js'
+import cookieParser from 'cookie-parser';
 configDotenv();
 
 mongoose
@@ -14,12 +15,15 @@ mongoose
     console.log("error");
   });
 
-const app = express();
+  const app = express();
+  app.use(express.json())
+  app.use(cookieParser());
+
+
 app.listen(3000, () => {
   console.log("server is running on port 3000");
 });
 
-app.use(express.json())
 app.use("/api/user", userRoutes);
 app.use("/api/auth",authRoutes);
 app.use((err,req,res,next)=>{
--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import { test } from '../controllers/user.controllers.js';
-
+import { updateUser } from '../controllers/user.controllers.js';
+import { verifyToken } from '../utils/verifyUser.js';
 // Create a router instance
 const router = express.Router();
 
 // Define routes
 router.get('/test',test);
+router.post('/update/:id',verifyToken,updateUser)
 
 // Export the router instance
 export default router;
