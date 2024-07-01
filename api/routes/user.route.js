f28a32f61f81ddfcd3b1d06dbe7814dbff7badd1
haseebonly <107769598+haseebonly@users.noreply.github.com>
2024-07-01 05:21:33 +0500

    add delete user functionality

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import { test } from '../controllers/user.controllers.js';
 import { updateUser } from '../controllers/user.controllers.js';
+import { deleteUser } from '../controllers/user.controllers.js';
 import { verifyToken } from '../utils/verifyUser.js';
 // Create a router instance
 const router = express.Router();
@@ -8,6 +9,8 @@ const router = express.Router();
 // Define routes
 router.get('/test',test);
 router.post('/update/:id',verifyToken,updateUser)
+router.delete('/delete/:id',verifyToken,deleteUser)
+
 
 // Export the router instance
 export default router;
