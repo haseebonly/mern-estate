ffddeddf28f665362590402b0785e18f4637bb8b
haseebonly <107769598+haseebonly@users.noreply.github.com>
2024-07-02 23:20:03 +0500

    complete delete user listing functionality

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,14 +1,15 @@
 import express from 'express';
-import { test,updateUser,deleteUser,getUserListings } from '../controllers/user.controllers.js';
+import { test,updateUser,deleteUser,getUserListings, getUser } from '../controllers/user.controllers.js';
 import { verifyToken } from '../utils/verifyUser.js';
 // Create a router instance
 const router = express.Router();
 
 // Define routes
-router.get('/test',test);
-router.post('/update/:id',verifyToken,updateUser)
-router.delete('/delete/:id',verifyToken,deleteUser)
-router.get('/listings/:id',verifyToken,getUserListings)
+router.get('/test', test);
+router.post('/update/:id', verifyToken, updateUser)
+router.delete('/delete/:id', verifyToken, deleteUser)
+router.get('/listings/:id', verifyToken, getUserListings)
+router.get('/:id', verifyToken, getUser)
 
 
 // Export the router instance
